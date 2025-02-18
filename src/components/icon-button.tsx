682227096c5f700eabcd6aee1a3f4e5318889b48
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-18 15:19:10 -0300

    Aula 01 - Configuração do projeto e componentes

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps, ReactNode } from 'react'
+
+interface IconButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+}
+
+export function IconButton(props: IconButtonProps) {
+  return (
+    <button
+      className="p-1.5 bg-gray-500 text-blue text-xl rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
+  )
+}
